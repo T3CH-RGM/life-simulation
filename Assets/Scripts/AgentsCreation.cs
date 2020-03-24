70beda2faf17071bcdf25b30444b200fb802bf92
Gaspar Habif <gasparhabif@gmail.com>
2020-03-24 19:26:30 -0300

    Initial commit

---
+++ Assets/Scripts/AgentsCreation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentsCreation : MonoBehaviour
+{
+    public GameObject prefab;
+    public int agentsAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < agentsAmount; i++)
+        {
+            Instantiate(prefab, new Vector3(0, i*1F, 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
