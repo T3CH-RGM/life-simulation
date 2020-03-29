95491db0026d4eb821fcb508b2b2a76215916238
Miguel Rodriguez <mmiguerodriguez@gmail.com>
2020-03-28 22:58:13 -0300

    Added food creation

---
+++ Assets/Scripts/FoodCreation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FoodCreation : MonoBehaviour
+{
+    public int foodAmount;
+    public GameObject foodObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < foodAmount; i++)
+        {
+            float xPos = Random.Range(-9.5f, 9.5f);
+            float yPos = Random.Range(-9.5f, 9.5f);
+            Instantiate(foodObject, new Vector3(xPos, 0.3F, yPos), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
